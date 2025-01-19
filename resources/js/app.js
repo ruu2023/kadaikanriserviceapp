a04e78969a4b2a027b5ddf486d334dfaed140610
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-01-20 01:31:56 +0900

    vueの読み込み修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,3 @@
 import "./bootstrap";
 import { createApp } from "vue";
 import ExampleComponent from "./components/ExampleComponent.vue";
-
-const app = createApp({});
-app.component("example-component", ExampleComponent);
-app.mount("#app");
